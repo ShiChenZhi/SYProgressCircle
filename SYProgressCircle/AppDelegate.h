cbc8b33036d0176e141d433a84996621a78cace3
mac <mac@shichenzhi.local>
2017-09-11 10:16:35 +0800

    Initial Commit

---
+++ SYProgressCircle/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SYProgressCircle
+//
+//  Created by shichenzhi on 2017/9/11.
+//  Copyright © 2017年 shichenzhi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SYProgressCircle/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SYProgressCircle
+//
+//  Created by shichenzhi on 2017/9/11.
+//  Copyright © 2017年 shichenzhi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
